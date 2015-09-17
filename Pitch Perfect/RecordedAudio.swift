19059f922f1e2ff71d011f56b5a8df49d5db0f3e
Trevor Adams <tadams@tcsiteservices.co.uk>
2015-09-17 22:23:44 +0100

    Created RecordedAudio class.

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Trevor Adams on 17/09/2015.
+//  Copyright © 2015 Trevor Adams. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio {
+    var filePathUrl: NSURL!
+    var title: String!
+}
